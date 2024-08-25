ceb58ef4c132f21a64b839b6eb144c0441a82aca
Tarn Jirayu <jaoaurai@gmail.com>
2024-08-26 01:00:17 +0700

    Update Cookie config

--- middleware.js
+++ middleware.js
@@ -1,18 +1,22 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-    // return NextResponse.redirect(new URL("/", request.url))
-    const res = NextResponse.next();
-    let expires = new Date(Date.now() + 10 * 1000);
-    res.cookies.set({
-        name: "session",
-        value: "xdd",
-        httpOnly: true,
-        expires: expires,
-      });
-    return res;
+  // return NextResponse.redirect(new URL("/", request.url))
+  const res = NextResponse.next();
+  let expires = new Date(Date.now() + 10 * 1000);
+  const session = request.cookies.get("session")?.value;
+  if (!session) {
+    console.log("no session")
+  }
+  res.cookies.set({
+    name: "session",
+    value: "session",
+    httpOnly: true,
+    expires: expires,
+  });
+  return res;
 }
 
 export const config = {
-    matcher: "/login(.*)"
-}
+  matcher: "",
+};
